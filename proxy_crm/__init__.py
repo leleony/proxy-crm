a393f445932880e00bcc845a3df947e2fa1fd40e
leleony <le.leony00@gmail.com>
2023-10-13 12:15:17 +0700

    Changing folder name and adding __init__.py

---
+++ proxy_crm/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from proxy_crm_modules import proxyCRM
+from fractionalFlow import frac_flow_fit, frac_flow_wor
+
+__all__ = ["proxyCRM", "frac_flow_fit", "frac_flow_wor"]
